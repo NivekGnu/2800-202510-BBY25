2fc5147ecfb56645b711d8a038c1d1c202fd9bc5
Shawn Lee <slee1109@my.bcit.ca>
2025-05-15 12:49:52 -0700

    Added CSS for 'createPost' page using Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./views/**/*.ejs",  // Path to your EJS templates
+    "./templates/**/*.ejs",
+    "./*.ejs",
     "./public/**/*.html", // HTML files (if any)
     "./src/**/*.js"      // JS files (if any)
   ],
